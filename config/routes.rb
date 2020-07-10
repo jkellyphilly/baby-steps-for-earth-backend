b3e1c55564d21b7fac324dbe0b5f85b5ecceb1da
jkellyphilly <joel.kelly.philly@gmail.com>
2020-07-10 14:56:14 -0400

    adding basic tag resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :tags
   resources :plans
   resources :goals
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
